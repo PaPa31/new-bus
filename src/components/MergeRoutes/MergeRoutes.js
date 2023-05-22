e20db0d856ae2e04f3192b35aba94f4a41db6494
papa <visidoor@yandex.ru>
2023-05-22 19:22:10 +0500

    rename `split/join` button

--- src/components/MergeRoutes/MergeRoutes.js
+++ src/components/MergeRoutes/MergeRoutes.js
@@ -3,7 +3,7 @@ import classes from "./MergeRoutes.module.css";
 
 const mergeRoutes = (props) => (
   <button className={classes.MergeRoutes} onClick={props.clicked}>
-    {props.toggle ? "Какой раньше" : "Какой чаще"}
+    {props.toggle ? "Объединить" : "Разделить"}
   </button>
 );
 
